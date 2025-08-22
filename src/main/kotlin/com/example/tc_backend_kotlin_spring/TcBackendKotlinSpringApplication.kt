f0aa9481d4fcee8412a76407a14974f5bd6c5d47
Erick Savluchinske <ericksav.dev@gmail.com>
2025-08-22 07:26:13 -0300

    initial commit

---
+++ src/main/kotlin/com/example/tc_backend_kotlin_spring/TcBackendKotlinSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tc_backend_kotlin_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TcBackendKotlinSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<TcBackendKotlinSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tc_backend_kotlin_spring/TcBackendKotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tc_backend_kotlin_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TcBackendKotlinSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
