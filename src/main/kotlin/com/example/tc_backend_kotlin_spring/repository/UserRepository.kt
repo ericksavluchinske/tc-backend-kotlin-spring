92dadf4e6dc3fe7c534f48c9f2ce8cc000ddaf8d
Erick Savluchinske <ericksav.dev@gmail.com>
2025-08-22 08:02:51 -0300

    create repository

---
+++ src/main/kotlin/com/example/tc_backend_kotlin_spring/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.example.tc_backend_kotlin_spring.repository
+
+import com.example.tc_backend_kotlin_spring.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+}
